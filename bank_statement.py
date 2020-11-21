164366fa639c704e1b74074ec69de9f72d18dabd
Md Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2020-11-21 15:40:26 +0600

    if else statement added in withdraw function

--- bank_statement.py
+++ bank_statement.py
@@ -8,12 +8,20 @@ class Bank():
 		self.balance = self.balance + amount
 		return f'{amount} transaction successful'
 	def withdraw(self,amount):
-		self.balance = self.balance - amount
-		return f'{amount} withdraw successful'
+		if self.balance>=amount:
+			self.balance = self.balance - amount
+			return f'{amount} withdraw successful'
+		else:
+			return 'You do not have enough balance'
 
 	def __str__(self):
 		return f'Name : {self.name}\nBalance: {self.balance}'
 
 account = Bank('Rezoan', 5000)
+print(account)
+
+# print(account)
+print(account.transaction(10000))
+print(account)
 print(account.withdraw(1000))
 print(account)
