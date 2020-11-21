43879ce623a69707f0729c4dda499101b8c70715
Md Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2020-11-21 16:03:13 +0600

    all problem solved by using else statement for the loop

--- prime_number.py
+++ prime_number.py
@@ -1,9 +1,9 @@
 # Prime or Composite
-num = 11
+num = 49
 for i in range(2,num):
 	if(num%i == 0):
 		print(f'{num} is not prime')
 		break
-	else:
-		print(f'{num} is prime')
-		break
+else:
+	print(f'{num} is prime')
+		
