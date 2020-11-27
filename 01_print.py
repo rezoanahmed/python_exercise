89b0aff674a19cd5197e220d86b981cd35d8ffd8
Md Rezoan Ahmed <58312828+rezoanahmed@users.noreply.github.com>
2020-11-27 10:32:28 +0600

    Delete 01_print.py

--- 01_print.py
+++
@@ -1,8 +0,0 @@
-# First python program using print function
-print('Hello, World!')
-print('Hello,\n World!')
-print('Hello,\t World!')
-print("It's the first program using print function")
-print('It\'s the first program using print function')
-
-# It's a comment and can not be executed
